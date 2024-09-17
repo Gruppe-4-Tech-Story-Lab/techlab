81dbada6dda091dfc6cf9266145db8f7b14fd763
Lasse <laje0005@stud.kea.dk>
2024-09-17 19:20:26 +0200

    :)

--- kategori.js
+++ kategori.js
@@ -1,23 +1,41 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
+const encoded = encodeURIComponent(category);
 console.log(category);
 
 // Fetch request til Supabase
-fetch(`https://hvqqqwpdmwotjgxebrxv.supabase.co/rest/v1/TSL?category=eq.${category}`, {
+fetch(`https://hvqqqwpdmwotjgxebrxv.supabase.co/rest/v1/TSLN?category=eq.${encoded}`, {
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2cXFxd3BkbXdvdGpneGVicnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2MDY4NzQsImV4cCI6MjA0MTE4Mjg3NH0.K9dKO1K4NQBdSCsqBZHh1Yc4L8VNHDBNgRCzsI0fSSQ",
   },
 })
   .then((res) => res.json())
-  .then(vis);
+  .then((data) => {
+    console.log(data)
+    const uniqueProducts = new Set();
+    const filteredData = data.filter((item) => {
+      if (uniqueProducts.has(item.produktnavn)) {
+        return false;
+      } else {
+        uniqueProducts.add(item.produktnavn);
+        return true;
+      }
+    });
+    vis(filteredData);
+    console.log(filteredData)
+  });
 
+// Funktion til at vise data på siden
 function vis(data) {
-  console.table(data);
-}
+  console.table(data); // Dette viser dataene i konsollen
 
-  //   data.forEach((element) => {
-  //     const link = document.createElement("a");
-  //     link.href = `underkategori.html?id=${element.id}`;
-  //     link.textContent = element.name;
-  //     document.body.appendChild(link);
-  //   });
+  data.forEach((item) => {
+    const template = document.querySelector("#smallProductTemplate").content;
+    const copy = template.cloneNode(true);
+
+    copy.querySelector("h3").textContent = item.produktnavn;
+    copy.querySelector(".subtle").textContent = item.type;
+
+    document.querySelector("main").appendChild(copy);
+  });
+}
